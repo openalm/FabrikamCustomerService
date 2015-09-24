4aff52b9a484c720180562c819715c378e418271
Anand Gaurav <agaurav.edu@live.com>
2015-09-24 15:44:35 +0530

    Project Add

---
+++ Fabrikam.CustomerService/FabrikamDB/dbo/Tables/Escalations.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Escalations]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Escalation] NCHAR(10) NULL, 
+    [Levels] NCHAR(10) NULL
+)
---
+++ README.md
@@ -0,0 +1,2 @@
+# CustomerService
+Fabrikam Fiber Customer Service
